91359c21b7a4e833a729b9cf3fbac07e8228b70b
GMartin-Data <gregory.martin.data@gmail.com>
2024-05-14 14:49:05 +0200

    Minimalist brew for db.py

--- src/db.py
+++ src/db.py
@@ -5,16 +5,10 @@ This file contains all the logic devoted to manage the database.
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
-from .models import Base, Account, Transaction
 
-
-def create_tables(engine):
-    # Will automatically check if the created tables exist.
-    Base.metadata.create_all(engine)
-
-def init_db_connection(connection_string):
+def init_db_connection():
     # It seems safer to return both even if you will mainly use
     # context managers like `with session:`
     # You will have to close all with `engine.dispose()`.
-    engine = create_engine(connection_string, echo=True)
-    return engine, Session(engine) 
+    engine = create_engine("sqlite:///bank.db", echo=True)
+    return engine, Session(engine)
