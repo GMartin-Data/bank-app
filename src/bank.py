175fff700a66df662b68a250f6279fdb8052f4ea
GMartin-Data <gregory.martin.data@gmail.com>
2024-05-14 05:44:49 +0200

    Add skeleton for classes

--- src/bank.py
+++ src/bank.py
@@ -10,3 +10,28 @@ This invoves two mandatory classes:
 - Account
 - Transaction
 """
+
+from datetime import datetime
+
+from sqlalchemy import Column, Float, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+
+from .init_db import Base
+
+
+# Classes
+class Account(Base):
+    __tablename__ = "accounts"
+    # Fields
+    ...
+    # Relationships
+    # One-to-Many: one account may have many transactions
+    pass
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+    # Fields
+    ...
+    # Relationships
+    # Many-to-One: any transaction has only one account
+    pass
