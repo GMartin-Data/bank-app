d828d6390b4334448a564d9c4841d687b8ce5828
GMartin-Data <gregory.martin.data@gmail.com>
2024-05-14 05:40:20 +0200

    Database initialization

--- src/init_db.py
+++ src/init_db.py
@@ -1,3 +1,12 @@
 """
 This file contains all the logic devoted to initialize the database, and manage connection.
 """
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, scoped_session, sessionmaker
+
+# init_db.py ###
+Base = declarative_base()
+engine = create_engine('sqlite:///library.db')
+Session = scoped_session(sessionmaker(bind=engine))
+Base.metadata.create_all(engine)
