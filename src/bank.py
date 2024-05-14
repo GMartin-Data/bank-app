233828b3ff7599ac075bd2da7174f7d8a9085cf4
GMartin-Data <gregory.martin.data@gmail.com>
2024-05-14 19:31:58 +0200

    withdraw reworked

--- src/bank.py
+++ src/bank.py
@@ -55,21 +55,79 @@ def deposit(session: Session, account_id: int, amount: float) -> None:
             # Register the corresponding transaction
             approved = Transaction(account_id, amount, "deposit")
             session.add(approved)
-            get_balance(session, account_id)
             session.commit()
             print(f"==> TRANSACTION {approved.transaction_id} APPROVED!")
+            
         except NoResultFound:
             print(f"/!\ TRANSACTION CANCELLED: There's no account with id {account_id}")
 
 
-def withdraw():
-    pass
+def withdraw(session: Session, account_id: int, amount: float) -> str:
+    if is_incorrect_amount(amount):
+        return "CANCELLED WITHDRAWAL: Incorrect amount prompted"
+    with session:
+        try:
+            account = (session
+                       .query(Account)
+                       .filter(Account.account_id == account_id)
+                       .one())
+            if account.balance < amount:
+                output = f"CANCELLED WITHDRAWAL: Insufficient funds on account {account_id}"
+                print(output)
+                return output
+            else:
+                account.balance -= amount
+                session.add(account)
+                # Register the corresponding transaction
+                approved = Transaction(account_id, amount, "withdraw")
+                session.add(approved)
+                session.commit()
+                output = f"APPROVED WITHDRAWAL - REF: {approved.transaction_id}"
+                print(output)
+                return output
 
-def transfer(account_id_from, account_id_to, amount):
-    # # Waiting for decorator
-    # engine, session = init_db_connection("sqlite:///bank.db")
-    # with session:
-    pass
+        except NoResultFound:
+            output = f"CANCELLED WITHDRAWAL: There's no account with id {account_id}"
+            print(output)
+            return output
+
+
+def transfer(session: Session,
+             account_from_id: int, account_to_id: int,
+             amount: float) -> None:
+    if is_incorrect_amount(amount):
+        return None
+    with session:
+        acc_from = (session
+                         .query(Account)
+                         .filter(Account.account_id == account_from_id)
+                         .all())
+        if not acc_from:
+            print(f"/!\ TRANSACTION CANCELLED: There's no account with id {account_from_id}")
+        else:
+            acc_from = acc_from[0]
+            if acc_from.balance < amount:
+                print(f"/!\ TRANSACTION CANCELLED: Insufficient funds on account {account_from_id}")
+            else:
+                acc_to = (session
+                          .query(Account)
+                          .filter(Account.account_id == account_from_id)
+                          .all())
+                if not acc_to:
+                    print(f"/!\ TRANSACTION CANCELLED: There's no account with id {account_to_id}")
+                else:
+                    acc_to = acc_to[0]
+                    acc_to.balance += amount
+                    session.add(acc_to)
+                    # Register the corresponding 2 transactions
+                    approved_from = Transaction(account_from_id, amount, "withdraw")
+                    approved_to = Transaction(account_to_id, amount, "deposit")
+                    session.add_all([approved_from, approved_to])
+                    get_balance(session, account_from_id)
+                    get_balance(session, account_to_id)
+                    session.commit()
+                    print(f"==> TRANSACTIONS {approved_from.transaction_id} and {approved_to.transaction_id} APPROVED!")
+
 
 def get_balance(session: Session, account_id: int) -> bool:
     with session:
@@ -86,10 +144,7 @@ def get_balance(session: Session, account_id: int) -> bool:
 
 if __name__ == "__main__":
     engine, session = init_db_connection()
-    # create_account(session, amount=0)
-    get_balance(session, account_id=2)
-    deposit(session, account_id=1, amount=-10)
-    deposit(session, account_id=2, amount="BOUH!")
-    deposit(session, account_id=3, amount=20)
-    deposit(session, account_id=1, amount=100)
+    withdraw(session, 1, 100)
+    print("#" * 150)
+    withdraw(session, 1, 500)
     engine.dispose()
