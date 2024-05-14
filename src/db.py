0ddb196e56627dfb9c5e919816c15d6a819d3f99
GMartin-Data <gregory.martin.data@gmail.com>
2024-05-14 12:25:04 +0200

    Implement create_tables and init_db_connection

--- src/db.py
+++ src/db.py
@@ -1,12 +1,26 @@
 """
-This file contains all the logic devoted to initialize the database, and manage connection.
+This file contains all the logic devoted to manage the database.
 """
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, scoped_session, sessionmaker
+from sqlalchemy.orm import Session
 
-# init_db.py ###
-Base = declarative_base()
-engine = create_engine('sqlite:///library.db')
-Session = scoped_session(sessionmaker(bind=engine))
-Base.metadata.create_all(engine)
+from .models import Base, Account, Transaction
+
+
+def create_tables(engine):
+    # Will automatically check if the created tables exist.
+    Base.metadata.create_all(engine)
+
+def init_db_connection(connection_string):
+    # It seems safer to return both even if you will mainly use
+    # context managers like `with session:`
+    # You will have to close all with `engine.dispose()`.
+    engine = create_engine(connection_string, echo=True)
+    return engine, Session(engine) 
+
+
+# # init_db.py ###
+# engine = create_engine('sqlite:///bank.db', echo=True)
+# Session = scoped_session(sessionmaker(bind=engine))
+# Base.metadata.create_all(engine)
