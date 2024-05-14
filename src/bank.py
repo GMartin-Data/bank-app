2d68c75cbe9657347a103cb0ff22eacb2f0baf7c
GMartin-Data <gregory.martin.data@gmail.com>
2024-05-14 15:28:59 +0200

    Functional implementation of get_balance

--- src/bank.py
+++ src/bank.py
@@ -13,15 +13,18 @@ This invoves two mandatory classes:
 
 from datetime import datetime
 
-from db import init_db_connection
+from sqlalchemy.orm import Session
+
 from models_legacy import Account
+from db import init_db_connection
 
 
 # Functions
-def create_account(amount: float = 0.0) -> None:
+def create_account(session: Session, amount: float = 0.0) -> None:
     new_account = Account(balance=amount)
-    session.add(new_account)
-    session.commit()
+    with session:
+        session.add(new_account)
+        session.commit()
 
 def deposit():
     pass
@@ -35,16 +38,17 @@ def transfer(account_id_from, account_id_to, amount):
     # with session:
     pass
 
-def get_balance():
-    pass
+def get_balance(session: Session, account_id: int) -> None:
+    with session:
+        account = (session
+                   .query(Account)
+                   .filter(Account.account_id == account_id)
+                   .one())
+        print(f"Account {account.account_id} has a balance of {account.balance}")
+    
 
 
 if __name__ == "__main__":
     engine, session = init_db_connection()
-    with session:
-        a = Account()
-        print("=====> Before db registering", a)
-        session.add(a)
-        session.commit()
-        print("=====> After db registering", a)
+    get_balance(session, 1)
     engine.dispose()
